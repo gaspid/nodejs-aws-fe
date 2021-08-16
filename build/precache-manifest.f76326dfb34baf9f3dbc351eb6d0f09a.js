5f7d2fedf995e4ae9f9d5a5308df4795ce31b8ce
gaspid <gaspid194@gmail.com>
2021-08-16 11:16:39 +0300

    init:task 2

---
+++ build/precache-manifest.f76326dfb34baf9f3dbc351eb6d0f09a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b0fd501be83b00b6f3117899a8586c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09e453516792ac3d450c",
+    "url": "/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "3388f02fbaea9331a3d2",
+    "url": "/static/js/2.fe8cd536.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/static/js/2.fe8cd536.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09e453516792ac3d450c",
+    "url": "/static/js/main.0d8b747b.chunk.js"
+  },
+  {
+    "revision": "e8a10cf83e6f7ed42a3a",
+    "url": "/static/js/runtime-main.b9870059.js"
+  }
+]);
